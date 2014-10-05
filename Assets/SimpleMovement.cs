e77faeb4ed2f625a3b9d2f1d213690f18f0a9f39
Darren <darren.royle@oup.com>
2014-10-05 11:36:23 +0100

    Updating to initial assets:

---
+++ Assets/SimpleMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour {
+	public float speed = 10.0F;
+	public float rotateSpeed = 1.0F;
+	void Update() {
+		transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		//Debug.Log (Vector3.right * 2);
+		if (Input.GetAxis ("Vertical") > 0) {
+				Vector3 m = Vector3.left * speed;
+				rigidbody.AddRelativeForce (m);
+		}
+	}
+}
